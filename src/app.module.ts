d2c7086c78228fc061a867bae29cb3f1ce4764d4
Jayden Pung <46307126+jaydenpung@users.noreply.github.com>
2022-08-01 23:58:32 +0800

    common helpers, typeorm connect mysql, serverless offline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      namingStrategy: new SnakeNamingStrategy(),
+    }),
+    CustomerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/lambda.ts
+++ src/lambda.ts
@@ -1,15 +1,30 @@
 import { configure as serverlessExpress } from '@vendia/serverless-express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filter/http-exception.filter';
+import { TransformInterceptor } from './common/interceptor/transform.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 let cachedServer;
 
 export const handler = async (event, context) => {
   if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    await nestApp.init();
+    const app = await NestFactory.create(AppModule);
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        transform: true,
+        transformOptions: {
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    app.useGlobalInterceptors(new TransformInterceptor());
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    await app.init();
     cachedServer = serverlessExpress({
-      app: nestApp.getHttpAdapter().getInstance(),
+      app: app.getHttpAdapter().getInstance(),
     });
   }
 
