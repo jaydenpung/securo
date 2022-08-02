e980273bebfe5b851b921138196e82d550820bd4
Jayden Pung <46307126+jaydenpung@users.noreply.github.com>
2022-08-03 06:49:27 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
 
-  await app.listen(8000);
+  await app.listen(process.env.PORT || 8000);
 }
 bootstrap();
